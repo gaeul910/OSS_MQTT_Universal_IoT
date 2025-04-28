b7ccc4438f3f61b0c455edaa67c5165d8b98c3bb
gaeul910 <chw910@chungbuk.ac.kr>
2025-04-28 21:31:33 +0900

    Location 관련 API 통합

--- Integration_Server/location.py
+++ Integration_Server/location.py
@@ -2,21 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/location/getdata")
+@app.route("/location/logs", methods=['GET', 'POST'])
 
-def getdata():
-    uid = request.args.get("uid", "str")
-    if(uid == "sample-uid"):
-        return render_template("location.json")
-    return "Request by UID is still under development"
+def logs():
+    if(request.method == 'GET'):
+        uid = request.args.get("uid", "str")
+        if(uid == "sample-uid"):
+            return render_template("location.json")
+        return "Request by UID is still under development"
+        
+    elif(request.method == 'POST'):
+        uid = request.args("uid", "str")
+        jsonoutput = open("templates/locationforupload.json", "w")
+        jsonoutput.write(request.data)
+        return
 
-@app.route("/location/postdata", methods=['post'])
-
-def postdata():
-    uid = request.args("uid", "str")
-    jsonoutput = open("templates/locationforupload.json", "w")
-    jsonoutput.write(request.data)
-    return
 
 if __name__ == "__main__":
     app.run(debug=True)
