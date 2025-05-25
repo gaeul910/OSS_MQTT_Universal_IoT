6a7db6d533d014806de23670a577bc547b696a0f
RyuGeonGil <fbrjsrlf@gmail.com>
2025-05-25 16:18:55 +0900

    서버와의 통신을 위한 notifications.dart의 변수 추가

---
+++ oss_iot_control_applicaton/lib/notifications.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  // 싱글톤 패턴
+  static final NotificationService _instance = NotificationService._internal();
+
+  factory NotificationService() => _instance;
+
+  NotificationService._internal();
+
+  // 상태 변수
+  bool _isPolling = false;
+  Isolate? _pollingIsolate;
+  ReceivePort? _receivePort;
+
+  // 서버 연결 정보
+  late String _ip;
+  late String _port;
+  late String _uid; // String으로 선언
+
+  final FlutterLocalNotificationsPlugin _notificationsPlugin = FlutterLocalNotificationsPlugin();
+}
