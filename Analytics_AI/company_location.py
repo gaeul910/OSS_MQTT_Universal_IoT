75dcc9767062a321ecc659a311498e272630e570
anhyo3539 <acg030809@naver.com>
2025-05-31 00:15:15 +0900

    처음 받은 데이터 변환

--- Analytics_AI/company_location.py
+++ Analytics_AI/company_location.py
@@ -7,3 +7,13 @@ import json
 import uuid
 import random
 import requests as r
+
+#처음 받은 데이터를 계산하기 쉽게 나누는 중
+def cluster_to_log_entries(cluster_store):
+    log_entries = []
+    for i, (cid, info) in enumerate(cluster_store.items(), start=1):
+        lat = round(info['lat'], 6)
+        lon = round(info['lon'], 6)
+    return log_entries
+    return log_entries
+
