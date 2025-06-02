0f94b3a498a0190ea0839035f6f3574c0a1bbc8f
anhyo3539 <acg030809@naver.com>
2025-06-02 17:30:09 +0900

    거리모듈 m로 치환

--- Analytics_AI/machine_swich.py
+++ Analytics_AI/machine_swich.py
@@ -2,7 +2,7 @@ import requests as r
 from datetime import datetime
 
 
-def swich(a,uid):
+def switch(a,uid):
     if a:
         machine_status="머신들이 작동을 시작하였습니다"
     else: 
@@ -14,4 +14,3 @@ def swich(a,uid):
     data = '{"content": "%s", "time": "%s", "about": 1}'%(machine_status,now)
     #res = r.post("http://localhost:3000/notification/postnoti", headers=headers,data=data)
     print(data,"  ",headers)
-
--- Analytics_AI/point_range.py
+++ Analytics_AI/point_range.py
@@ -1,8 +1,14 @@
-import math 
+from math import radians, sin, cos, sqrt, atan2
 
 def range(point1,point2):
-    length=math.dist(point1,point2)
-    return length 
+    R = 6371 * 1000  # 지구 반지름 (미터 단위)
+    
+    dlat = radians(point2[1] - point1[1])
+    dlon = radians(point2[0] - point1[0])
+    
+    a = sin(dlat/2)**2 + cos(radians(point1[1])) * cos(radians(point2[1])) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return R * c
 
 def slope(point1,point2):
     if(point2[0]-point1[0]==0):
